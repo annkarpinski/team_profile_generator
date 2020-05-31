33a5a6fd911e5acba5518a1e0bef599ecb8459e5
Ann Karpinski <annkarpinski@gmail.com>
2020-05-31 15:43:46 -0600

    built out classes for each employee type and got all tests to pass

--- Develop/app.js
+++ Develop/app.js
@@ -10,9 +10,73 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the employee's name?",
+      name: "employee",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the URL to your project?",
+      name: "url",
+    },
+    {
+      type: "input",
+      message: "What is your project's name?",
+      name: "projectName",
+    },
+    {
+      type: "input",
+      message: "Please write a short description of your project:",
+      name: "description",
+    },
+    {
+      type: "list",
+      message: "What kind of license should your project have?",
+      name: "license",
+      choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
+    },
+    {
+      type: "input",
+      message: "What command should be run to install dependencies?",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "What command should be run to run tests?",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "What does the user need to know about using the repo?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message:
+        "What does the user need to know about contributing to the repo?",
+      name: "contribute",
+    },
+  ])
+  .then(function (data) {
+    console.log(data);
+
+    const newEmployee = new Manager(
+      data.name,
+      data.id,
+      data.email,
+      data.officeNumber
+    );
+  });
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
